2c1a133cf3bf0b13c751f9f397b5ed413d5a1c87
Rohit Kumawat <rohitkumavatgame@gmail.com>
2024-12-19 14:50:47 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -5,17 +5,25 @@ class player:
         self.power=100
         self.defence=0
     
-    def punch(self):
+    def punch(self,enemy):
         if self.power<10:
             return False
         else:
+            enemy.hp-=10
             self.power-=10
     
-    def kick(self):
+    def kick(self,enemy):
         if self.power<20:
             return False
         else:
+            enemy.hp-=20
             self.power-=20
+
+    def hp_checker(self):
+        if self.hp==0:
+            return False
+        return True
+
 
 class enemy:
     def __init__(self,hp,atk):
@@ -27,18 +35,37 @@ class enemy:
             return False
         else:
             player.hp-=self.atk
+    
+    def hp_checker(self):
+        if self.hp<0:
+            return False
+        return True
 
 
 rohit=player()
-e1=enemy(100,20)
+e1=enemy(10,0)
 while True:
-    a=int(input("Enter choice  "))
-    if a==1:
-        rohit.punch()
-        e1.attack(rohit)
-        print(f"our hp is {rohit.hp} left")
-        print(rohit.power)
+    if rohit.hp_checker()==False:
+            print("you lose")
+            break
+    elif e1.hp_checker()==False:
+        print("You win")
+        break
     else:
-        rohit.kick()
-        print(rohit.power)
+        a=int(input("Enter choice  "))
+        if a==1:
+            if rohit.punch(e1)==False:
+                print("not enough power ")
+            else:
+                rohit.punch(e1)
+                e1.attack(rohit)
+                print(f" hp {rohit.hp} left")
+                print(f"ur Power is {rohit.power} ")
+                print(f"Enemy hp is {e1.hp} left")
+        
+        else:
+            rohit.kick(e1)
+            e1.attack(rohit)
+            print(f"our hp is {rohit.hp} left")
+            print(f"Enemy hp is {e1.hp} left")
         
